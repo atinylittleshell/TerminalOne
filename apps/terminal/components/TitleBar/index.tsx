20104ff5e69c6ecaeb72f5a0ab6520e58d707083
kunchenguid <kunchenguid@gmail.com>
2023-04-24 20:23:49 -0700

    mac title bar

--- apps/terminal/components/TitleBar/index.tsx
+++ apps/terminal/components/TitleBar/index.tsx
@@ -15,56 +15,54 @@ function TitleBar(props: PropsWithChildren<{}>) {
     });
   }, []);
 
-  if (window.TerminalOne.platform === 'darwin') {
-    return (
-      <div className="flex flex-row item-center h-7 relative bg-base-300">
-        <div className={`block absolute -z-10 top-0 left-0 right-0 bottom-0 ${styles['title-bar-drag']}`} />
-      </div>
-    );
-  }
-
   return (
-    <div className="flex flex-row item-center h-8 relative bg-base-300">
+    <div
+      className={`flex flex-row item-center relative bg-base-300 ${
+        window.TerminalOne.platform === 'darwin' ? 'h-16 pt-8' : 'h-8'
+      }`}
+    >
       <div className={`block absolute -z-10 top-0 left-0 right-0 bottom-0 ${styles['title-bar-drag']}`} />
       <div className={`${styles['title-bar-buttons']}`}>{props.children}</div>
       <div className="flex-1" />
-      <div className={`flex flex-row text-white ${styles['title-bar-buttons']}`}>
-        <button
-          className="btn btn-sm btn-ghost btn-square"
-          title="Minimize"
-          onClick={() => {
-            window.TerminalOne.win?.minimize();
-          }}
-        >
-          <FiMinus size="16" />
-        </button>
-        <button
-          className="btn btn-sm btn-ghost btn-square"
-          title="Maximize"
-          onClick={() => {
-            if (isMaximized) {
-              window.TerminalOne.win?.maximize(false);
-              setIsMaximized(false);
-            } else {
-              window.TerminalOne.win?.maximize(true);
-              setIsMaximized(true);
-            }
-          }}
-        >
-          {isMaximized ? <FiMinimize2 size="16" /> : <FiMaximize2 size="16" />}
-        </button>
-        <button
-          className="btn btn-sm btn-ghost btn-square hover:bg-error"
-          title="Close"
-          onClick={async () => {
-            flushSync(() => {
-              window.TerminalOne.app?.quit();
-            });
-          }}
-        >
-          <FiX size="16" />
-        </button>
-      </div>
+      {window.TerminalOne.platform === 'darwin' ? null : (
+        <div className={`flex flex-row text-white ${styles['title-bar-buttons']}`}>
+          <button
+            className="btn btn-sm btn-ghost btn-square"
+            title="Minimize"
+            onClick={() => {
+              window.TerminalOne.win?.minimize();
+            }}
+          >
+            <FiMinus size="16" />
+          </button>
+          <button
+            className="btn btn-sm btn-ghost btn-square"
+            title="Maximize"
+            onClick={() => {
+              if (isMaximized) {
+                window.TerminalOne.win?.maximize(false);
+                setIsMaximized(false);
+              } else {
+                window.TerminalOne.win?.maximize(true);
+                setIsMaximized(true);
+              }
+            }}
+          >
+            {isMaximized ? <FiMinimize2 size="16" /> : <FiMaximize2 size="16" />}
+          </button>
+          <button
+            className="btn btn-sm btn-ghost btn-square hover:bg-error"
+            title="Close"
+            onClick={async () => {
+              flushSync(() => {
+                window.TerminalOne.app?.quit();
+              });
+            }}
+          >
+            <FiX size="16" />
+          </button>
+        </div>
+      )}
     </div>
   );
 }
