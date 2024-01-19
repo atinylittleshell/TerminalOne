64ccf79ea446f16e7d6cdb4b20c493a5dd977a89
Kun Chen <kunchenguid@gmail.com>
2024-01-19 00:14:31 -0800

    close tab when shell exits

--- apps/terminal/components/Tab/index.tsx
+++ apps/terminal/components/Tab/index.tsx
@@ -3,12 +3,20 @@ import _ from 'lodash';
 import { TabContextProvider } from '../../hooks/TabContext';
 import { TerminalTreeNode } from '../TerminalTreeNode';
 
-const Tab = ({ tabId, active }: { tabId: number; active: boolean }) => {
+const Tab = ({
+  tabId,
+  active,
+  close,
+}: {
+  tabId: number;
+  active: boolean;
+  close: () => void;
+}) => {
   return (
     <div
       className={`w-full h-full absolute ${active ? 'visible' : 'invisible'}`}
     >
-      <TabContextProvider active={active} tabId={tabId}>
+      <TabContextProvider active={active} tabId={tabId} close={close}>
         <TerminalTreeNode data={null} />
       </TabContextProvider>
     </div>
