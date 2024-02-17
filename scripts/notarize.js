7c7d0c13916ec44f4aff94ff8f10bea2afa0a87e
atinylittleshell <3233006+atinylittleshell@users.noreply.github.com>
2024-02-16 22:48:10 -0800

    codesign mac build (#53)
    * move packages
    
    * codesign mac build

---
+++ scripts/notarize.js
@@ -0,0 +1,22 @@
+/* eslint-disable turbo/no-undeclared-env-vars */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { notarize } = require('@electron/notarize');
+
+exports.default = async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;
+  if (
+    electronPlatformName !== 'darwin' ||
+    !process.env.APPLE_ID ||
+    !process.env.APPLE_PASSWORD
+  ) {
+    return;
+  }
+
+  const appName = context.packager.appInfo.productFilename;
+  return await notarize({
+    appBundleId: 'io.terminalone.app',
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: process.env.APPLE_ID,
+    appleIdPassword: process.env.APPLE_PASSWORD,
+  });
+};
