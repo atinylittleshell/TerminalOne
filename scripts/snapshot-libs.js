c5bcf0508b7c0aeff03f39c7680dc1a424731c2b
Kun Chen <kunchenguid@gmail.com>
2024-02-17 13:59:13 -0800

    v8 snapshot

--- scripts/snapshot-libs.js
+++ scripts/snapshot-libs.js
@@ -2,14 +2,14 @@ require('daisyui');
 require('dayjs');
 require('eventemitter3');
 require('lodash');
-require('next');
-require('react-icons');
 require('xterm-addon-unicode11');
 // eslint-disable-next-line no-constant-condition
 if (false) {
   require('@tailwindcss/typography');
-  require('react-dom');
+  require('next');
   require('react');
+  require('react-dom');
+  require('react-icons');
   require('xterm-addon-fit');
   require('xterm-addon-web-links');
   require('xterm-addon-webgl');
